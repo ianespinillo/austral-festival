f0dcec1b6f0f50c8e78c4aae03cce6df0e1fb714
Ian Espinillo <ianespinillo@gmail.com>
2026-09-21 18:22:10 -0300

    fix(consts): fixed campus link and street number

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_ALCOHOL_ALLOWANCE = 3;
 
-export const VENUE_ADDRESS = "Mariano Acosta 1610 · Pilar · Universidad Austral";
+export const VENUE_ADDRESS = "Mariano Acosta 1611 · Pilar · Universidad Austral";
 export const VENUE_MAPS_URL =
-  "https://www.google.com/maps/search/?api=1&query=Mariano%20Acosta%201610%2C%20Pilar%2C%20Argentina";
+  "https://maps.app.goo.gl/bPzrpnsijTw9VMCr9";
 export const DOORS_TEXT = "Apertura 20:00 hs · Cierre 22:00 hs";
 export const PREVENTA_LABEL = "Preventa 1";
